c3f0fb2c75a492b23f9c5c0b09105b960f7b5371
hajra <rizviha@sheridancollege.ca>
2021-06-05 21:20:33 -0400

    Step 12 CardTrick asks the user to a pick a card and then searches if the user card matches the cards in random array

--- src/hajra/rizvi/s991627316/CardTrick.java
+++ src/hajra/rizvi/s991627316/CardTrick.java
@@ -1,5 +1,7 @@
 package hajra.rizvi.s991627316;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
@@ -12,7 +14,7 @@ package hajra.rizvi.s991627316;
 public class CardTrick {
 
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
         Card[] magicHand = new Card[7];
 
         for (int i = 0; i < magicHand.length; i++) {
@@ -28,10 +30,43 @@ public class CardTrick {
             System.out.println(magicHands.getSuit() + " " + magicHands.getValue());
         }
         
-
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Let's Begin!");
+        System.out.println("Pick a card! Any Card!");
+        System.out.print("Pick the card value (1-13): ");
+        int inputValue = in.nextInt();
+        System.out.print("Pick the card suit (Hearts = 1, Diamonds = 2, "
+                + "Spades = 3, Clubs = 4): ");
+        int inputSuit = in.nextInt();
+        
+        String userSuit;
+        if (inputSuit == 1){
+            userSuit = "Hearts";
+        } else if (inputSuit == 2){
+            userSuit = "Diamonds";
+        } else if (inputSuit == 3){
+            userSuit = "Spades";
+        } else {
+            userSuit = "Clubs";
+        }
+        
+        System.out.println("Your Card: " + inputValue + " of " 
+                + userSuit);
+        
+        
         // and search magicHand here
+        String finalValue = "You Lose!";
+        for (Card magicHands : magicHand){
+            if (inputValue == magicHands.getValue() && userSuit.equals(magicHands.getSuit())){
+                finalValue = "You Win!";
+            }
+        }
         //Then report the result here
-    }
+        System.out.println(finalValue);    
+        
+        
 
+    }
 }
+
+
